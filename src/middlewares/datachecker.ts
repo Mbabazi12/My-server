0e380ec4d0f820f111f5b74130424939b23bdb0d
mbabazi12 <mbabazi069@gmail.com>
2024-03-09 01:29:46 +0200

    fix: errors debugging

--- src/middlewares/datachecker.ts
+++ src/middlewares/datachecker.ts
@@ -2,31 +2,29 @@ import express, {Request,Response, NextFunction } from "express";
 import {User} from "../model/user";
 import { errorMessage } from "../utils/errorMessage";
 class dataChecker{
-    public static async inputIsEmpty(req:Request, res:Response,next : NextFunction) :Promise<Response> {
+    public static async inputIsEmpty(req:Request, res:Response,next : NextFunction) :Promise<void> {
     const {username,email,passWord} = req.body
     if(username ==""){
-        return res.status(401).json({ status:'failed', error : 'username required'});
+        return errorMessage(res,401,`username is empty please fill in something`)
     }else if(email == ""){
-        return res.status(401).json({ status:'failed', error : 'Email required'});
+        return errorMessage(res,401,`email is empty please fill in the email`)
     }
     else if(passWord == ""){
-        return res.status(401).json({ status:'failed', error : 'Password required'});
+        return errorMessage(res,401,`password is empty please fill`)
     }
     else{
         next()
     }
-    return res.status(201).json({ status:'success',});
-}
+    }
 
-    public static async EmailExist(req: Request, res: Response, next: NextFunction): Promise<Response> {
+    public static async EmailExist(req: Request, res: Response, next: NextFunction): Promise<void> {
         const { email } = req.body;
         const user = await User.findOne({ email });
         if (user) {
-            return res.status(401).json({ status:'failed', error : 'Email already exist'});
+            return errorMessage(res, 404, `Email already exists`);
         } else {
             next();
         }
-        return res.status(201).json({ status:'success',});
     }    
 }
 export { dataChecker }
